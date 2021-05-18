b0bff8cb28d566c036129911a58549e0f0198524
AnupamMisra <innocentangelfromhell@gmail.com>
2021-05-18 21:39:25 +0530

    worked

--- deployment.py
+++ deployment.py
@@ -1,2 +1,5 @@
+from datetime import datetime
 import streamlit as st
 st.write("Hello world. This is my first streamlit project")
+dat=st.date_input("When do you want to take flight",datetime.date(2021,5,19))
+st.write(f'Date entered: {dat}')

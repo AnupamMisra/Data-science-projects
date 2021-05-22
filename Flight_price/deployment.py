08dddbd8fb99ddb89d6a8da2dff6777229271a94
AnupamMisra <innocentangelfromhell@gmail.com>
2021-05-22 23:18:17 +0530

    created route option and kept only direct flights

--- Flight_price/deployment.py
+++ Flight_price/deployment.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 import streamlit as st
 st.write("Hello world. This is my first streamlit project")
-dat=st.date_input("When do you want to take flight",datetime.date(2021,5,19))
+dat=st.date_input("When are you planning to take the flight",datetime(2021,5,19))
 st.write(f'Date entered: {dat}')
+
+src=st.text_input("Enter source")
+dest=st.text_input("Enter destination")
+airline = st.selectbox('Preferred airline',('Air Asia', 'GoAir', 'IndiGo', 'Multiple carriers','SpiceJet','Vistara'))
+st.write(f'Looking for flights from {src} to {dest} via {airline} on {dat}... ')

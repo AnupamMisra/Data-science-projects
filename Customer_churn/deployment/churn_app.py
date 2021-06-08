50d377f22f687288b2151b3b79d1b3a461717795
AnupamMisra <innocentangelfromhell@gmail.com>
2021-06-08 20:24:34 +0530

    downloading and uploading template succesfully!!!

--- Customer_churn/deployment/churn_app.py
+++ Customer_churn/deployment/churn_app.py
@@ -1,60 +1,47 @@
 import streamlit as st
 import pandas as pd
+import base64
+from io import BytesIO
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1',index=False)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    #return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="churn_template.csv">Download template for uploading</a>' # decode b'abc' => abc
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="churn_template.csv">Download template for uploading</a>' # decode b'abc' => abc
+
+def uploader():
+    vals = [123,'Male/Female','Yes/No','Yes/No','Yes/No',123,'Yes/No','Yes/No/No phone service','DSL/Fibre optic/No','Yes/No/No internet sevice','Yes/No/No internet sevice','Yes/No/No internet sevice','Yes/No/No internet sevice','Yes/No/No internet sevice','Yes/No/No internet sevice','Month-to-month/One year/Two year','Yes/No','Electronic check/Mailed check/Bank transfer (automatic)/Credit card (automatic)',123,123]
+    d= pd.Series(vals, index=['CustomerID','gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure','PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity','OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV','StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod','MonthlyCharges', 'TotalCharges'])
+    d=pd.DataFrame(d).transpose()
+    
+    st.markdown(get_table_download_link(d), unsafe_allow_html=True)
+    uploaded_file=st.file_uploader("Please upload data in the provided format")
+    
+    if uploaded_file is not None:
+        df=pd.read_excel(uploaded_file, skiprows=1)
+        return df
+
+    else:
+        st.text("Waiting for your input...")    
+
+if __name__=='__main__':
+    
+    st.title("Customer churn prediction module")
+
+    df=uploader()
+    if df is not None:
+        st.write(df.iloc[0,:])
 
 
-gender=st.selectbox('Gender',('Male','Female'))
-senior=st.checkbox('Senior citizen')
-partner=st.checkbox('In a relationship?')
-dependents=st.checkbox('Do you have dependents?')
-phone=st.checkbox('Phone service',('Yes','No'))
-if phone:
-    multiple=st.checkbox('Multiple lines?')
-else: multiple=-1
-
-internet=st.selectbox('Internet service',('No','DSL', 'Fiber optic'))
-
-if internet!='No':
-
-    security=st.checkbox('Online security')
-    backup=st.checkbox('Online backup')
-    protection=st.checkbox('Device protection')
-    support=st.checkbox('Tech support')
-    tv=st.checkbox('Streaming TV?')
-    movies=st.checkbox('Streaming movies')
-
-else:
-    security=-1
-    backup=-1
-    protection=-1
-    support=-1
-    tv=-1
-    movies=-1
-
-contract=st.selectbox('Contract duration',('Month-to-month', 'One year', 'Two year'))
-paperless=st.checkbox('Paperless billing?')
-payment=st.selectbox('Payment mode',('Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'))
-
-tenure=st.slider('Tenure(months)', min_value=0, max_value=1000,step=1)
-monthly=st.slider('Monthly charges', min_value=100, max_value=1000,step=25)
-total=st.slider('Total charges', min_value=100, max_value=5000,step=50)
-
-'''
-def preprocess(X):
-    lb=LabelBinarizer()
-
-    binary_feat = X.nunique()[X.nunique() == 2].keys().tolist()
-    numeric_feat = [col for col in X.select_dtypes(['float','int']).columns.tolist() if col not in binary_feat]
-    categorical_feat = [ col for col in X.select_dtypes('object').columns.to_list() if col not in binary_feat + numeric_feat ]
-
-    #le = LabelEncoder()
-    for i in binary_feat:
-    X[i] = lb.fit_transform(X[i])
-
-    X = pd.get_dummies(X, columns=categorical_feat)
-    sc=StandardScaler()
-    X = sc.fit_transform(X)
-
-    X=pd.DataFrame(X)
-    return X
-
-'''

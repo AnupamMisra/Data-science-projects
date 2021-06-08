4e0421025d2a1814320241a7e99822ff313c338b
AnupamMisra <innocentangelfromhell@gmail.com>
2021-06-08 22:16:49 +0530

    Model is predicting, but all non churn.

--- Customer_churn/deployment/churn_app.py
+++ Customer_churn/deployment/churn_app.py
@@ -32,73 +32,74 @@ def uploader():
     uploaded_file=st.file_uploader("Please upload data in the provided format")
     
     if uploaded_file is not None:
-        df=pd.read_excel(uploaded_file)
-        df=df[df.CustomerID!=0]
-        return df
+        dff=pd.read_excel(uploaded_file)
+        dff=dff[dff.CustomerID!=0]
+        return dff
 
     else:
         st.text("Waiting for your input...")    
 
 def preprocess(df):
 
-    X['gender'] = df.gender=='Male'
-    X['SeniorCitizen'] = df.SeniorCitizen=='Yes'
-    X['Partner'] = df.Partner=='Yes'
-    X['Dependents'] = df.Dependents=='Yes'
-    X['PhoneService'] = df.PhoneService=='Yes'
-    X['PaperlessBilling'] = df.PaperlessBilling=='Yes'
-    X['MultipleLines_No'] = df.MultipleLines=="No"
-    X['MultipleLines_Yes'] = df.MultipleLines=="Yes"
-    X['MultipleLines_No phone service'] = df.MultipleLines=="No phone service"
-    X['InternetService_Fiber optic'] = df.InternetService=="Fibre optic"
-    X['InternetService_No'] = df.InternetService=="No"
-    X['InternetService_DSL'] = df.InternetService=="DSL"
-    X['OnlineSecurity_No'] = df.OnlineSecurity=="No"
-    X['OnlineSecurity_No internet service'] = df.OnlineSecurity=="No internet service"
-    X['OnlineSecurity_Yes'] = df.OnlineSecurity=="Yes"
-    X['OnlineBackup_No'] = df.OnlineBackup=='No'
-    X['OnlineBackup_No internet service'] = df.OnlineBackup=='No internet service'
-    X['OnlineBackup_Yes'] = df.OnlineBackup=='Yes'
-    X['DeviceProtection_No'] = df.DeviceProtection=="No"
-    X['DeviceProtection_No internet service'] = df.DeviceProtection=="No internet service"
-    X['DeviceProtection_Yes'] = df.DeviceProtection=="Yes"
-    X['TechSupport_No internet service'] = df.TechSupport=="No internet service"
-    X['TechSupport_Yes']= df.TechSupport=="Yes"
-    X['TechSupport_No']= df.TechSupport=="No"
-    X['StreamingTV_No'] = df.StreamingTV=="No"
-    X['StreamingTV_No internet service'] = df.StreamingTV=="No internet service"
-    X['StreamingTV_Yes'] = df.StreamingTV=="Yes"
-    X['StreamingMovies_No'] = df.StreamingMovies=="No"
-    X['StreamingMovies_No internet service'] = df.StreamingMovies=="No internet service"
-    X['StreamingMovies_Yes'] = df.StreamingMovies=="Yes"
-    X['Contract_Month-to-month']=df.Contract=='Month-to-month'
-    X['Contract_One year']=df.Contract=='One year'
-    X['Contract_Two year']=df.Contract=='Two year'
-    X['PaymentMethod_Bank transfer (automatic)'] = df.PaymentMethod=="Bank transfer (automatic)"
-    X['PaymentMethod_Credit card (automatic)'] = df.PaymentMethod=="Credit card (automatic)"
-    X['PaymentMethod_Electronic check'] = df.PaymentMethod=="Electronic check"
-    X['PaymentMethod_Mailed check'] = df.PaymentMethod=="Mailed check"
+    X=pd.DataFrame()
+    X['gender'] = (df.gender=='Male').astype('int')
+    X['SeniorCitizen'] = (df.SeniorCitizen=='Yes').astype('int')
+    X['Partner'] =( df.Partner=='Yes').astype('int')
+    X['Dependents'] = (df.Dependents=='Yes').astype('int')
+    X['PhoneService'] = (df.PhoneService=='Yes').astype('int')
+    X['PaperlessBilling'] = (df.PaperlessBilling=='Yes').astype('int')
+    X['MultipleLines_No'] = (df.MultipleLines=="No").astype('int')
+    X['MultipleLines_Yes'] = (df.MultipleLines=="Yes").astype('int')
+    X['MultipleLines_No phone service'] = (df.MultipleLines=="No phone service").astype('int')
+    X['InternetService_Fiber optic'] = (df.InternetService=="Fibre optic").astype('int')
+    X['InternetService_No'] =( df.InternetService=="No").astype('int')
+    X['InternetService_DSL'] = (df.InternetService=="DSL").astype('int')
+    X['OnlineSecurity_No'] = (df.OnlineSecurity=="No").astype('int')
+    X['OnlineSecurity_No internet service'] = (df.OnlineSecurity=="No internet service").astype('int')
+    X['OnlineSecurity_Yes'] =( df.OnlineSecurity=="Yes").astype('int')
+    X['OnlineBackup_No'] = (df.OnlineBackup=='No').astype('int')
+    X['OnlineBackup_No internet service'] = (df.OnlineBackup=='No internet service').astype('int')
+    X['OnlineBackup_Yes'] = (df.OnlineBackup=='Yes').astype('int')
+    X['DeviceProtection_No'] = (df.DeviceProtection=="No").astype('int')
+    X['DeviceProtection_No internet service'] = (df.DeviceProtection=="No internet service").astype('int')
+    X['DeviceProtection_Yes'] =( df.DeviceProtection=="Yes").astype('int')
+    X['TechSupport_No internet service'] = (df.TechSupport=="No internet service").astype('int')
+    X['TechSupport_Yes']= (df.TechSupport=="Yes").astype('int')
+    X['TechSupport_No']= (df.TechSupport=="No").astype('int')
+    X['StreamingTV_No'] =( df.StreamingTV=="No").astype('int')
+    X['StreamingTV_No internet service'] = (df.StreamingTV=="No internet service").astype('int')
+    X['StreamingTV_Yes'] = (df.StreamingTV=="Yes").astype('int')
+    X['StreamingMovies_No'] = (df.StreamingMovies=="No").astype('int')
+    X['StreamingMovies_No internet service'] = (df.StreamingMovies=="No internet service").astype('int')
+    X['StreamingMovies_Yes'] = (df.StreamingMovies=="Yes").astype('int')
+    X['Contract_Month-to-month']=(df.Contract=='Month-to-month').astype('int')
+    X['Contract_One year']=(df.Contract=='One year').astype('int')
+    X['Contract_Two year']=(df.Contract=='Two year').astype('int')
+    X['PaymentMethod_Bank transfer (automatic)'] = (df.PaymentMethod=="Bank transfer (automatic)").astype('int')
+    X['PaymentMethod_Credit card (automatic)'] = (df.PaymentMethod=="Credit card (automatic)").astype('int')
+    X['PaymentMethod_Electronic check'] = (df.PaymentMethod=="Electronic check").astype('int')
+    X['PaymentMethod_Mailed check'] = (df.PaymentMethod=="Mailed check").astype('int')
 
     with open('../deployment/scalar','rb') as a:
         scalar=pickle.load(a)
 
     X[['tenure','MonthlyCharges','TotalCharges']] = scalar.transform(df[['tenure','MonthlyCharges','TotalCharges']])
-
+    df.drop(['MultipleLines', 'InternetService','OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport','StreamingTV', 'StreamingMovies', 'Contract','PaymentMethod'],axis=1,inplace=True)
     return X
  
 if __name__=='__main__':
     
     st.title("Phone company's churn prediction module for the Martian Sapiens")
 
-    df=uploader()
-    if df is not None:
-        X=df.drop(['CustomerID'],axis=1)
-        custID=df['CustomerID']
-
+    dff=uploader()
+    if dff is not None:
+        df=dff.drop(['CustomerID'],axis=1)
+        custID=dff['CustomerID']
+        
         c=st.slider("Cost of churn", min_value=500, max_value=10000,step=500)
         e=st.slider("Cost of preventing churn", min_value=0, max_value=5000,step=50)
 
-        model_frame=preprocess(X)
+        model_frame=preprocess(df)
 
         with open('../deployment/rf','rb') as a:
             rf=pickle.load(a)
@@ -108,4 +109,9 @@ if __name__=='__main__':
             g=pickle.load(a)            
 
         y=rf.predict(model_frame)
-        st.write(y)
+        out=pd.DataFrame(y,index=custID)
+        out.columns=['Churn']
+        out.index.name='Customer ID'
+        st.write(out)
+
+         # Do overall analysis   

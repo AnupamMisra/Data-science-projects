2b37885e3f480594c48ed3b10df3992de4bebae7
AnupamMisra <innocentangelfromhell@gmail.com>
2021-06-09 14:57:09 +0530

    model_path_issue

--- churn_app.py
+++ churn_app.py
@@ -97,7 +97,7 @@ if __name__=='__main__':
        
         model_frame=preprocess(df)
         model_frame=model_frame.reindex(['gender','SeniorCitizen','Partner', 'Dependents',  'PhoneService', 'PaperlessBilling', 'MultipleLines_No', 'MultipleLines_No phone service', 'MultipleLines_Yes','InternetService_DSL', 'InternetService_Fiber optic', 'InternetService_No', 'OnlineSecurity_No', 'OnlineSecurity_No internet service', 'OnlineSecurity_Yes', 'OnlineBackup_No', 'OnlineBackup_No internet service', 'OnlineBackup_Yes', 'DeviceProtection_No', 'DeviceProtection_No internet service', 'DeviceProtection_Yes', 'TechSupport_No', 'TechSupport_No internet service', 'TechSupport_Yes', 'StreamingTV_No', 'StreamingTV_No internet service', 'StreamingTV_Yes',  'StreamingMovies_No', 'StreamingMovies_No internet service', 'StreamingMovies_Yes', 'Contract_Month-to-month', 'Contract_One year', 'Contract_Two year', 'PaymentMethod_Bank transfer (automatic)', 'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check','TotalCharges','MonthlyCharges','tenure'],axis=1)
-        with open('../deployment/model','rb') as a:
+        with open('./Customer_churn/deployment/model','rb') as a:
             model=pickle.load(a)
            
 
